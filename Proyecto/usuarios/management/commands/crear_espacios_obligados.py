ae332d917860cc1fc172ef3e7ae447ec152d5f38
JeremiasMartin <jmtnalv@gmail.com>
2023-12-11 17:57:32 -0300

    Se crean espacios obligados de acuerdo a las sedes existentes

--- Proyecto/usuarios/management/commands/crear_espacios_obligados.py
+++ Proyecto/usuarios/management/commands/crear_espacios_obligados.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from espacios_obligados.models import EspacioObligado
+from espacios_obligados.models import EspacioObligado, Sede
 from faker import Faker
 import random
 
@@ -8,12 +8,28 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         fake = Faker('es_AR')
-        num_registros = 1
+        num_registros = 100
+
+        # Obtener todas las sedes existentes
+        sedes_disponibles = list(Sede.objects.all())
 
         for _ in range(num_registros):
+            # Verificar si hay sedes existentes
+            if not sedes_disponibles:
+                break
+
+            # Seleccionar una sede aleatoria para el EspacioObligado
+            sede = random.choice(sedes_disponibles)
+
+            # Crear el EspacioObligado y asociarlo a la sede
             espacio_obligado = EspacioObligado.objects.create(
                 estado=random.choice(["CARDIO ASISTIDO CERTIFICADO", "EN PROCESO", "CARDIO ASISTIDO", "CARDIO ASISTIDO CON CERTIFICADO VENCIDO"]),
-                motivo=fake.text()
+                motivo=fake.text(),
+                sede=sede
             )
+
+            # Eliminar la sede seleccionada de las sedes disponibles, 
+            # para evitar que se repitan y termine quedando espacios_obligados = sedes con respecto a la cantidad
+            sedes_disponibles.remove(sede)
 
         self.stdout.write(self.style.SUCCESS(f"Se generaron y guardaron {num_registros} registros en la tabla EspacioObligado"))
